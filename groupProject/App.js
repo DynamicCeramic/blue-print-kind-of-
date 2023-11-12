d58e024390b1fb74d97efecc64937bd4cd9359de
Karma63 <71836970+Karma63@users.noreply.github.com>
2023-11-12 12:15:44 +0000

    Update App.js
    added the start of the booking

--- groupProject/App.js
+++ groupProject/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import {
   StyleSheet,
   Text,
@@ -7,7 +7,9 @@ import {
   TouchableOpacity,
   Button,
   Alert,
+  TextInput,
 } from 'react-native';
+import DatePicker from 'react-native-date-picker';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -18,8 +20,7 @@ const Stack = createNativeStackNavigator();
 function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <View
-        style={{ alignItems: 'center', justifyContent: 'flex-start', flex: 1 }}>
+      <View style={{ alignItems: 'center', justifyContent: 'flex-start', flex: 1 }}>
         <Text>Home Screen</Text>
         <Button
           title="Go to booking"
@@ -30,8 +31,8 @@ function HomeScreen({ navigation }) {
           onPress={() => navigation.navigate('Health')}
         />
         <Button
-          title="go to the blog post page"
-          onPress={() => navigation.navigate('blog')}
+          title="Go to the blog post page"
+          onPress={() => navigation.navigate('Blog')}
         />
         <Text style={{ fontWeight: 'bold', fontSize: 30 }}>
           Welcome to Aberdeen ceramic and our{' '}
@@ -42,25 +43,95 @@ function HomeScreen({ navigation }) {
   );
 }
 
-function Bookingscreen({navigation}) {
+function BookingScreen({ navigation }) {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [hours, setHours] = useState('');
+  const [showDatePicker, setShowDatePicker] = useState(false);
+
+  const handleDateChange = useCallback(() => {
+    setShowDatePicker(true);
+  }, []);
+
+  const handleBooking = useCallback(() => {
+    if (!hours) {
+      Alert.alert('Please enter the number of hours.');
+      return;
+    }
+
+    const parsedHours = parseFloat(hours);
+
+    if (isNaN(parsedHours) || parsedHours <= 0 || parsedHours > 4) {
+      Alert.alert('Please enter a valid number of hours (1 to 4).');
+      return;
+    }
+
+    // Log selected date and hours to the console
+    console.log('Selected Date:', selectedDate.toDateString());
+    console.log('Selected Hours:', parsedHours);
+
+    // For actual implementation, replace the console.log with your booking logic
+    // ...
+
+    // Clear input fields
+    setSelectedDate(new Date());
+    setHours('');
+  }, [hours, selectedDate]);
+
+  const handleDatePickerChange = useCallback((date) => {
+    setShowDatePicker(false);
+    setSelectedDate(date);
+  }, []);
+
+  const handleDatePickerCancel = useCallback(() => {
+    setShowDatePicker(false);
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Booking Screen</Text>
+        <TouchableOpacity onPress={handleDateChange}>
+          <Text style={styles.datePickerText}>
+            {selectedDate.toDateString()}
+          </Text>
+        </TouchableOpacity>
+        {showDatePicker && (
+          <DatePicker
+            date={selectedDate}
+            mode="date"
+            onDateChange={handleDatePickerChange}
+            onCancel={handleDatePickerCancel}
+          />
+        )}
+        <TextInput
+          style={{
+            height: 40,
+            borderColor: 'gray',
+            borderWidth: 1,
+            marginTop: 20,
+            width: 200,
+            textAlign: 'center',
+          }}
+          placeholder="Enter hours"
+          keyboardType="numeric"
+          value={hours}
+          onChangeText={(text) => setHours(text)}
+        />
+        <Button title="Book Now" onPress={handleBooking} />
         <Button
           title="Go to health and safety screen"
           onPress={() => navigation.navigate('Health')}
         />
         <Button
-          title="go to the blog post page"
-          onPress={() => navigation.navigate('blog')}
+          title="Go to the blog post page"
+          onPress={() => navigation.navigate('Blog')}
         />
       </View>
     </View>
   );
 }
 
-function HealthScreen({navigation}) {
+function HealthScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Button
@@ -68,14 +139,14 @@ function HealthScreen({navigation}) {
         onPress={() => navigation.navigate('Booking')}
       />
       <Button
-        title="go to the blog post page"
-        onPress={() => navigation.navigate('blog')}
+        title="Go to the blog post page"
+        onPress={() => navigation.navigate('Blog')}
       />
     </View>
   );
 }
 
-function BlogScreen({navigation}) {
+function BlogScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Button
@@ -92,11 +163,14 @@ function BlogScreen({navigation}) {
 
 const OpenURLButton = ({ url, children }) => {
   const handlePress = useCallback(async () => {
+    // Check if the device supports opening the URL
     const supported = await Linking.canOpenURL(url);
 
     if (supported) {
+      // Open the URL if supported
       await Linking.openURL(url);
     } else {
+      // Show an alert if the URL cannot be opened
       Alert.alert(`Don't know how to open this URL: ${url}`);
     }
   }, [url]);
@@ -121,9 +195,9 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Booking" component={Bookingscreen} />
+        <Stack.Screen name="Booking" component={BookingScreen} />
         <Stack.Screen name="Health" component={HealthScreen} />
-        <Stack.Screen name="blog" component={BlogScreen} />
+        <Stack.Screen name="Blog" component={BlogScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -135,5 +209,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#999ea2',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  datePickerText: {
+    fontSize: 20,
+    color: 'blue',
+    textDecorationLine: 'underline',
+    marginTop: 20,
   },
 });
